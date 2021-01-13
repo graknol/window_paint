14d29f34efebd4a84ba79a49bda6d06e839095a8
Sindre van der Linden <graknol@gmail.com>
2021-01-13 07:44:48 +0100

    Comment formatting.

--- lib/src/draw/draw_object_adapter.dart
+++ lib/src/draw/draw_object_adapter.dart
@@ -24,18 +24,15 @@ abstract class DrawObjectAdapter<T extends DrawObject> {
 
   /// Returning [true] will trigger a re-paint.
   ///
-  /// This is useful for things like pan/zoom
-  /// where you don't paint anything.
+  /// This is useful for things like pan/zoom where you don't paint anything.
   ///
   /// NOTE: Will not be called if [start] returned a [Future].
   bool update(T object, Offset focalPoint, Color color);
 
-  /// Returning [true] will keep the object,
-  /// [false] will discard it.
+  /// Returning [true] will keep the object, [false] will discard it.
   ///
-  /// This is useful for things like pan/zoom
-  /// where you want to fulfill the contract,
-  /// but not draw anything.
+  /// This is useful for things like pan/zoom where you want to fulfill the
+  /// contract, but not draw anything.
   ///
   /// NOTE: Will not be called if [start] returned a [Future].
   bool end(T object, Color color);
